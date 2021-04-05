9099de98747292f8cebfb48a2b96e3acab52bad2
guokeyf <guoke_yf@163.com>
2021-04-06 02:12:07 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,61 @@
 <template>
   <div class="todo-container">
     <div class="todo-wrap">
+      <Header @addItem="addItem"/>
+      <List :todos="todos" :deleteItem="deleteItem" v-show="this.todos.length"/>
+      <Footer :todos="todos" :deleteAllComplete="deleteAllComplete" :selectAllTodos="selectAllTodos" v-show="this.todos.length"/>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import Header from "./components/Header";
+import List from "./components/List";
+import Footer from "./components/Footer";
+export default {
+  data () {
+    return {
+      todos: JSON.parse(localStorage.getItem('todo_key') || '[]')
+    }
+  },
+  watch: {
+    todos: {
+      deep:true,
+      handler: function (value) {
+        localStorage.setItem('todo_key',JSON.stringify(value))
+      }
+    }
+  },
+  methods: {
+    addItem (todo) {
+      this.todos.unshift(todo)
+    },
+    deleteItem (index) {
+      this.todos.splice(index, 1)
+    },
+    deleteAllComplete () {
+      this.todos = this.todos.filter(todo => !todo.complete)
+    },
+    selectAllTodos (check) {
+      this.todos.forEach(todo => todo.complete = check)
+    }
+  },
+  components: {
+    Header,
+    List,
+    Footer
+  }
+}
 </script>
 
 <style>
-
+.todo-container {
+  width: 600px;
+  margin: 0 auto;
+}
+.todo-container .todo-wrap {
+  padding: 10px;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,49 @@
 <template>
   <div class="todo-header">
-    <input type="text" placeholder="请输入你的任务名称，按回车键确认"/>
+    <input type="text" placeholder="请输入你的任务名称，按回车键确认" @keyup.enter="addTodo" v-model="title"/>
   </div>
 
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      title: '',
+      complete: false
+    }
+  },
+  methods: {
+    addTodo () {
+      const title = this.title
+      if (!title) {
+        alert('请正确输入！！！')
+        return
+      }
+      const todo = {
+        title,
+        complete: false
+      }
+      this.$emit('addItem',todo)
+      this.title = ''
+    }
+  }
+}
 </script>
 
 <style>
+.todo-header input {
+  width: 560px;
+  height: 28px;
+  font-size: 14px;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  padding: 4px 7px;
+}
 
+.todo-header input:focus {
+  outline: none;
+  border-color: rgba(82, 168, 236, 0.8);
+  box-shadow: inset 0 1px 1px rgba(0, 0, 0, 0.075), 0 0 8px rgba(82, 168, 236, 0.6);
+}
 </style>
